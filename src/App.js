addcb7c23d6582fe30edc8febc273d57bf05ec11
Dominik Mrówka <dmrowka7@gmail.com>
2022-09-07 13:28:43 +0200

    Add variables in scss, change input type, add info about wrong input number

--- src/App.js
+++ src/App.js
@@ -12,41 +12,41 @@ const App = () => {
   const [tipAmount, setTipAmount] = useState('0.00');
   const [total, setTotal] = useState('0.00');
   const [active, setActive] = useState('');
-  const [customTip, setCustomTip] = useState('')
+  const [customTip, setCustomTip] = useState('');
 
 
   const handleCount = (e) => {
     setNumOfPpl(e)
-      const numberOfPeople = e;
+    const numberOfPeople = e;
+    let tipCorrectValue
 
     if (customTip === '') {
-      const tipCorrectValue = Number(tip.lastIndexOf('%') > 0 ? tip.slice(0, -1) : tip);
-      const tipAmount = Number(bill) * tipCorrectValue / 100
-      const total = (Number(bill) + (Number(bill) * tipCorrectValue / 100)) / Number(numberOfPeople)
-      const totalResult = total.toFixed(2)
-      numberOfPeople !== '' ? setTotal(totalResult) : setTotal('0.00')
-      const tipAmountPerPerson = (tipAmount / numberOfPeople).toFixed(2);
-      numberOfPeople !== '' ? setTipAmount(tipAmountPerPerson) : setTipAmount('0.00')
+      tipCorrectValue = Number(tip.lastIndexOf('%') > 0 ? tip.slice(0, -1) : tip);
     } else {
-      const tipAmount = Number(bill) * customTip / 100
-      const total = (Number(bill) + (Number(bill) * customTip / 100)) / Number(numberOfPeople)
-      const totalResult = total.toFixed(2)
-      numberOfPeople !== '' ? setTotal(totalResult) : setTotal('0.00')
-      const tipAmountPerPerson = (tipAmount / numberOfPeople).toFixed(2);
-      numberOfPeople !== '' ? setTipAmount(tipAmountPerPerson) : setTipAmount('0.00')
+      tipCorrectValue = customTip
       setActive('');
-    }
+    };
 
+    const tipAmount = Number(bill) * tipCorrectValue / 100
+    const total = (Number(bill) + (Number(bill) * tipCorrectValue / 100)) / Number(numberOfPeople)
+    const totalResult = total.toFixed(2)
+    numberOfPeople !== '' && numberOfPeople != 0 ? setTotal(totalResult) : setTotal('0.00')
+    const tipAmountPerPerson = (tipAmount / numberOfPeople).toFixed(2);
+    numberOfPeople !== '' && numberOfPeople != 0 ? setTipAmount(tipAmountPerPerson) : setTipAmount('0.00')
+    console.log(tipCorrectValue);
   };
 
   const selectTip = (value) => {
-    setActive(value)
-    setTip(value)
+    setActive(value);
+    setTip(value);
+    setCustomTip('');
+    handleCount(numOfPpl)
   };
 
   const handleCustomTip = (value) => {
     setCustomTip(value);
     setActive('');
+    setTip('');
   };
 
   const handleReset = () => {
@@ -59,19 +59,19 @@ const App = () => {
     setActive('');
   };
 
-
-
   const numOfButton = ['5%', '10%','15%','25%','50%']
 
   return (
     <>
+
       <div className="bill-box">
         <div className="left-box">
           <div className="bill">
             <span className="title">Bill</span>
+            {(bill < 0 || customTip < 0 || numOfPpl < 0) && <span className="wrong-value">Wrong value in { bill < 0 && 'Bill' || customTip < 0 && 'Custom Tip' || numOfPpl < 0 && 'Number of People'}!</span>}
             <div className="bill-price">
               <span>$</span>
-              <input type="text" placeholder='0' onChange={(e) => setBill(e.target.value)} value={bill} />
+              <input type="number" placeholder='0' onChange={(e) => setBill(e.target.value)} value={bill} />
             </div>
           </div>
           <div className="tip-select">
@@ -80,12 +80,12 @@ const App = () => {
               {numOfButton.map((button) => {
                 return <div key={button} id={button} className={`tile ${active === button ? 'active' : ''}`} onClick={(e)=> selectTip(e.target.innerText)}>{button}</div>
               })}
-              <input type="text" className="tile" placeholder='Custom' onChange={(e) => handleCustomTip(e.target.value)} value={customTip} />
+              <input type="number" className="tile" placeholder='Custom' onChange={(e) => handleCustomTip(e.target.value)} value={customTip} />
             </div>
           </div>
           <div className="num-of-ppl">
             <span className="title">Number of People</span>
-            <input type="text" placeholder='0' onChange={(e)=>handleCount(e.target.value)} value={numOfPpl} />
+            <input type="number" placeholder='0' onChange={(e)=>handleCount(e.target.value)} value={numOfPpl} />
           </div>
         </div>
         <div className="right-box">
